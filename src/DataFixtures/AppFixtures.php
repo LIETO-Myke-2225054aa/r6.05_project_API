19e78188e2a4c70fe7779f6d075910e4c00ac272
lieto <myke.lieto@etu.univ-amu.fr>
2025-02-16 21:40:06 +0100

    Last commit

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\UserProfile;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -11,6 +11,13 @@ class AppFixtures extends Fixture
     {
         // $product = new Product();
         // $manager->persist($product);
+
+        for ($i = 1; $i <= 100; $i++) {
+            $userProfile = new UserProfile();
+            $userProfile->setEmail("user$i@example.com");
+
+            $manager->persist($userProfile);
+        }
 
         $manager->flush();
     }
